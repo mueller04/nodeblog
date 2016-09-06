1fc8936ec19326fd99116b5722092a43ed3bc664
Mike Mueller <mikemueller04@hotmail.com>
2016-09-06 15:31:10 -0400

    created blog entry

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var email = 'mikemuellerware@gmail.com';
+var subject = '?Subject=Blog%20Site%20Reader'
+var mailtoemail = 'mailto:' + email + subject;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Node Blog' });
@@ -11,7 +16,10 @@ router.get('/about', function(req, res, next) {
 })
 
 router.get('/contact', function(req, res, next) {
-    res.render('contact', {title: 'Contact', email: 'mikemuellerware@gmail.com'});
+    console.log(mailtoemail);
+    res.render('contact', {title: 'Contact',
+        email: email,
+        mailtoemail: mailtoemail });
 })
 
 module.exports = router;
